34fc042f3eb8e721ebae52ca605c7d3acefb13e9
MaksymchukA <nastena_1991@ukr.net>
2023-01-21 18:14:38 +0200

    did first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const changeColorBody = document.querySelector('body')
 
+const DELAY = 1000;
+let idInterval = null;
+
+btnStart.addEventListener('click', onBtnStartClick);
+btnStop.addEventListener('click', onBtnStoptClick);
+
+function onBtnStartClick() {
+    idInterval = setInterval(() => {
+        changeColorBody.style.backgroundColor = getRandomHexColor()
+    }, DELAY);
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+}
+
+function onBtnStoptClick() {
+    clearInterval(idInterval);
+
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
